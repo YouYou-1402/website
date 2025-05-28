76337e7ab9abc01dcb1732119aa49b86846b51ce
YouYou-1402 <clapika1402@gmail.com>
2025-05-28 21:25:42 +0700

    2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from './hooks/useAuth';
 import Layout from './components/Layout/Layout';
 import ProtectedRoute from './components/Auth/ProtectedRoute';
 
@@ -22,50 +22,54 @@ import NotFound from './pages/NotFound';
 // Styles
 import './styles/index.css';
 
+// Layout Wrapper Component
+const LayoutWrapper = ({ children }) => (
+  <Layout>{children}</Layout>
+);
+
+// Protected Layout Wrapper
+const ProtectedLayoutWrapper = ({ children }) => (
+  <Layout>
+    <ProtectedRoute>
+      {children}
+    </ProtectedRoute>
+  </Layout>
+);
+
 function App() {
   return (
     <AuthProvider>
       <Router>
         <div className="App">
           <Routes>
-            {/* Auth Routes (without layout) */}
+            {/* Auth Routes - Không có Layout */}
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             
-            {/* Main Routes (with layout) */}
-            <Route path="/*" element={
-              <Layout>
-                <Routes>
-                  <Route index element={<Home />} />
-                  
-                  {/* Books */}
-                  <Route path="books" element={<BooksList />} />
-                  <Route path="books/:id" element={<BookDetail />} />
-                  
-                  {/* Movies */}
-                  <Route path="movies" element={<MoviesList />} />
-                  <Route path="movies/:id" element={<MovieDetail />} />
-                  
-                  {/* Music */}
-                  <Route path="music" element={<MusicList />} />
-                  <Route path="music/:id" element={<MusicDetail />} />
-                  
-                  {/* Blog */}
-                  <Route path="blog" element={<BlogList />} />
-                  <Route path="blog/:id" element={<BlogDetail />} />
-                  
-                  {/* Protected Routes */}
-                  <Route path="profile" element={
-                    <ProtectedRoute>
-                      <Profile />
-                    </ProtectedRoute>
-                  } />
-                  
-                  {/* 404 */}
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
-              </Layout>
-            } />
+            {/* Public Routes với Layout */}
+            <Route path="/" element={<LayoutWrapper><Home /></LayoutWrapper>} />
+            
+            {/* Books */}
+            <Route path="/books" element={<LayoutWrapper><BooksList /></LayoutWrapper>} />
+            <Route path="/books/:id" element={<LayoutWrapper><BookDetail /></LayoutWrapper>} />
+            
+            {/* Movies */}
+            <Route path="/movies" element={<LayoutWrapper><MoviesList /></LayoutWrapper>} />
+            <Route path="/movies/:id" element={<LayoutWrapper><MovieDetail /></LayoutWrapper>} />
+            
+            {/* Music */}
+            <Route path="/music" element={<LayoutWrapper><MusicList /></LayoutWrapper>} />
+            <Route path="/music/:id" element={<LayoutWrapper><MusicDetail /></LayoutWrapper>} />
+            
+            {/* Blog */}
+            <Route path="/blog" element={<LayoutWrapper><BlogList /></LayoutWrapper>} />
+            <Route path="/blog/:id" element={<LayoutWrapper><BlogDetail /></LayoutWrapper>} />
+            
+            {/* Protected Routes */}
+            <Route path="/profile" element={<ProtectedLayoutWrapper><Profile /></ProtectedLayoutWrapper>} />
+            
+            {/* 404 */}
+            <Route path="*" element={<LayoutWrapper><NotFound /></LayoutWrapper>} />
           </Routes>
         </div>
       </Router>
